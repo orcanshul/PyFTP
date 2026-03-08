68bd8f676f7d54385a4cd74b1d6a14c95246f2db
Orcanshul <Anshul0402@gmail.com>
2026-03-07 16:32:22 -0800

    feat: new encryption logic, as well as added FastAPI UI elements (js / react)

--- app.py
+++ app.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+import hashlib 
 
+text = "hello world"
+hash_object = hashlib.sha256(text.encode())
+hash_digest = hash_object.hexdigest()
+print("SHA Hash of", text , "is ", hash_digest)
 
 @app.get("/")
 def root():
-    return {"Hello": "World"}
+    def hash_file(file_path):
+        h = hashlib.new("sha256")
+        with open(file_path, "rb") as file: 
+            while True:
+                chunk = file.read(1024)
+                if chunk == b"": 
+                    break 
+                h.update(chunk)
+        return h.hexdigest()
